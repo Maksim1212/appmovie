fcc5c63340916f713a5ad01c0a79fb39c4dffa07
Maksim1212 <balukov.maks@inbox.ru>
2025-04-19 08:42:26 -0100

    Bottom Navigation(Jetpack Navigation Component)

--- app/src/main/java/com/example/appmovie/MainActivity.kt
+++ app/src/main/java/com/example/appmovie/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.appmovie
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomnavigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.searchFragment,
+                R.id.favoriteFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+    }*/
     }
 }
---
+++ app/src/main/java/com/example/appmovie/movie/presentation/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.appmovie.movie.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.appmovie.R
+import com.example.appmovie.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
