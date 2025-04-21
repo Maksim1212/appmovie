0dade69910b5e33ad1738468eb5699ef11cc446b
Maksim1212 <balukov.maks@inbox.ru>
2025-04-21 16:53:17 -0100

    MOVIE-7: fix bottom navigation

--- app/src/main/java/com/example/appmovie/movie/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/appmovie/movie/presentation/favorite/FavoriteFragment.kt
@@ -8,20 +8,12 @@ import androidx.fragment.app.Fragment
 import com.example.appmovie.R
 
 class FavoriteFragment : Fragment() {
-    private var _binding: FragmentFavoriteBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFavoriteBinding.inflate(inflater,container,false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
 }
