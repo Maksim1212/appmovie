9c5f7236baa1ea635da09eec0529d09d1b9b2fe1
Maksim1212 <balukov.maks@inbox.ru>
2025-04-26 16:15:34 -0100

    MOVIE-9: 1.Realization diffUtil,2. Realization ItemDecoration, 3.The distance between the layouts,4. Add a layout description

---
+++ app/src/main/java/com/example/appmovie/FilmsItemDecoration.kt
@@ -0,0 +1,20 @@
+package com.example.appmovie
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class FilmsItemDecoration(private val film: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = film
+        outRect.right = film
+        outRect.bottom = film
+
+        if (parent.getChildAdapterPosition(view) == 0) {
+            outRect.top = film
+        } else {
+            outRect.top = 24
+        }
+    }
+}
+//мяугг
